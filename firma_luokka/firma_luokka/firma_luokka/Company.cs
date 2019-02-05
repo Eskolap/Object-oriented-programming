6c83809a20b5a3662807a8bae9ec5a83ae117e19
Eskolap <42930020+Eskolap@users.noreply.github.com>
2019-02-05 08:38:37 +0200

    Add files via upload

---
+++ firma_luokka/firma_luokka/firma_luokka/Company.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace firma_luokka
+{
+    class Company
+    {
+        public string Title;
+        public string Address;
+        public string Phone;
+        public int Income;
+        public int Expense;
+        public Company()
+        {
+            
+        }
+        public Company(string title, string address, string phone, int income, int expense)
+        {
+            this.Title = title;
+            this.Address = address;
+            this.Phone = phone;
+            this.Income = income;
+            this.Expense = expense;
+        }
+        public string profit()
+        {
+            string s = string.Empty;
+            decimal profit = (this.Income - this.Expense) / this.Expense;
+            if (profit < 1)
+
+                s = "Menee kehnosti";
+            else if (profit <= 2)
+                s = "Menee välttävästi";
+            else if (profit <= 3)
+                s = "Menee tyydyttävästi";
+            else
+                s = "Menee hyvin";
+            return $"Voittoprosentti on: {profit.ToString("p")}. {s}";
+            
+
+        }
+
+        public override string ToString()
+        {
+            return "Firma:\t" + this.Title + "\nOsoite:\t" + this.Address + "\nPuhelin:" + this.Phone + "\nTulot:\t" + this.Income.ToString("c", CultureInfo.CurrentCulture) + "\nMenot: \t" + this.Expense.ToString("c", CultureInfo.CurrentCulture);
+
+        }
+
+        public Company(Company copyCompany)
+        {
+            Title = copyCompany.Title;
+            Address = copyCompany.Address;
+            Phone = copyCompany.Phone;
+            Income = copyCompany.Income;
+            Expense = copyCompany.Expense;
+
+        }
+
+    }
+}
---
+++ firma_luokka/firma_luokka/firma_luokka/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace firma_luokka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Firman tiedot");
+            Console.WriteLine("----------------------------");
+            Company company = new Company("Lika-Akin makasiini", "Akinkuja 9", "0405040521", 10250, 9124);
+            Company enterprise = new Company(company);
+            Console.WriteLine(company.ToString());
+
+            Console.WriteLine(company.profit());
+            Console.WriteLine("\nKopiointi:\n");
+            Console.WriteLine(enterprise.ToString());
+
+
+        }
+    }
+}
